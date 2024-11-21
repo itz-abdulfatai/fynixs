e4c45694ea6fc7baf331abad20b4b132926535bf
itz_abdulfatai <134066346+itz-abdulfatai@users.noreply.github.com>
2024-11-21 12:49:50 +0100

    edit service padding

--- frontend/src/pages/ServicesView.jsx
+++ frontend/src/pages/ServicesView.jsx
@@ -26,7 +26,7 @@ function ServicesView() {
   return (
     <>
     <Header location="services"/>
-    <section className=" flex flex-col md:flex-row md:justify-between items-center justify-center gap-10 min-h-[83vh] ">
+    <section className=" flex flex-col lg:flex-row md:justify-between pt-24 items-center justify-center gap-10 min-h-[83vh] ">
       <h1 className=" text-2xl md:text-5xl max-w-full">{service.name}  </h1>
       <div className=" flex flex-col justify-center gap-5">
         {
